70b7ea280eb816aec98e808cf76d5474e39cb98a
Yegor <kutuzov-1313@yandex.ru>
2021-03-13 13:20:30 +0300

    rename variable

--- fetch_spacex.py
+++ fetch_spacex.py
@@ -16,7 +16,7 @@ def download_images_spacex():
     parser = create_parser()
     args = parser.parse_args()
     spacex_launch_id = args.id
-    spacex_path_images = args.download_path
+    path_download_images = args.download_path
 
     api_spacex = f'https://api.spacexdata.com/v3/launches/{spacex_launch_id}/'
 
@@ -24,9 +24,9 @@ def download_images_spacex():
     response.raise_for_status()
 
     spacex_image_urls = response.json()['links']['flickr_images']
-    for number_url, spacex_url in enumerate(spacex_image_urls):
-        spacex_filename = f'spacex{number_url}{get_file_extension(spacex_url)}'
-        download_image(spacex_filename, spacex_url, spacex_path_images)
+    for id_url_image, link_url_image in enumerate(spacex_image_urls):
+        spacex_filename = f'spacex{id_url_image}{get_file_extension(link_url_image)}'
+        download_image(spacex_filename, link_url_image, path_download_images)
 
 
 if __name__ == '__main__':
